c85b0462fc0b7973c8caa9f9f3ba814ed04fb85c
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2025-04-05 16:30:19 +0530

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,10 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+// middleware.ts
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware({
-  publicRoutes : ['/', ]
-})
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/not-found"],
+});
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
